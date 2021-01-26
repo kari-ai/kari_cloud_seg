8d1296b349fc8fd7fb3de4a24f00a8553f3c1adc
Han Oh <ohhan777@gmail.com>
2021-01-26 13:21:18 +0900

    avoid mixed precision in loss function

--- loss.py
+++ loss.py
@@ -68,7 +68,7 @@ def dice_loss(true, logits, eps=1e-7):
         true_1_hot = F.one_hot(true.squeeze(1), num_classes=num_classes)   # (B, 1, H, W) to (B, H, W, C)
         true_1_hot = true_1_hot.permute(0, 3, 1, 2)                        # (B, H, W, C) to (B, C, H, W)
         probas = F.softmax(logits, dim=1)
-    true_1_hot = true_1_hot.type(logits.type())
+    true_1_hot = true_1_hot.type(logits.type()).contiguous()
     dims = (0,) + tuple(range(2, true.ndimension()))        # dims = (0, 2, 3)
     intersection = torch.sum(probas * true_1_hot, dims)     # intersection w.r.t. the class
     cardinality = torch.sum(probas + true_1_hot, dims)      # cardinality w.r.t. the class
@@ -103,7 +103,7 @@ def jaccard_loss(true, logits, eps=1e-7):
         true_1_hot = F.one_hot(true.squeeze(1), num_classes=num_classes)  # (B, 1, H, W) to (B, H, W, C)
         true_1_hot = true_1_hot.permute(0, 3, 1, 2)  # (B, H, W, C) to (B, C, H, W)
         probas = F.softmax(logits, dim=1)
-    true_1_hot = true_1_hot.type(logits.type())
+    true_1_hot = true_1_hot.type(logits.type()).contiguous()
     dims = (0,) + tuple(range(2, true.ndimension()))
     intersection = torch.sum(probas * true_1_hot, dims)
     cardinality = torch.sum(probas + true_1_hot, dims)
